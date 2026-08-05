75df93154d50b032e5c58400bedcb7b26aad16f8
Labham Jadhav <labham04@gmail.com>
2026-08-05 23:27:11 +0530

    Update Terraform configuration

--- demo.tf
+++ demo.tf
@@ -37,5 +37,21 @@ resource "aws_s3_bucket" "my_bucket" {
 
 -----------
 
+provider "aws" {
+  region = "ap-south-1"
+}
 
+resource "aws_instance" "my_ec2" {
+  ami                    = "ami-01a00762f46d584a1"
+  instance_type          = "t3.micro"
+  key_name               = "my-keypair"
+  vpc_security_group_ids = ["sg-044c77041f14f0e45"]
+
+  tags = {
+    Name = "MyEC2"
+  }
+}
+
+
+-----------
 
